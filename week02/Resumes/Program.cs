6863bfff349e4a4937255b36a7f7b70bd2e33970
rsupreus <danasupreus@outlook.com>
2026-07-16 16:13:18 -0600

    week02 learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Account Manager";
+        job1._company = "PioneerRx";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Product Support Engineer";
+        job2._company = "Courier Plus, Inc.";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+
+        myResume._name = "RoseDana Supreus";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
